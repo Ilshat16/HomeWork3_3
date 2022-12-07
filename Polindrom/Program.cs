7407a21860880c83c50ae4169e6d218d114e8806
Ilshat <illliat@mail.ru>
2022-12-07 21:16:31 +0300

    Palindrom

---
+++ Polindrom/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число
+//и проверяет, является ли оно палиндромом
+
+//Методы
+void IsPalindrom(int number)
+{
+    string num = "" + number;
+    int length = num.Length;
+    string result = $"Число {number} палиндром";
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (num[i] != num[length - 1 - i]) 
+        {
+            result = $"Число {number} не палиндром";
+            break;
+        }
+    }
+    Console.WriteLine(result);
+}
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+//Дано
+int number = EnterData("Введите число: ");
+//Выполнение
+IsPalindrom(number);
